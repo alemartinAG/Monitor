25ffa24a18b415d05ea1f7e2bd0d920574e21523
alemartinAG <ale.martin96@hotmail.com>
2019-08-25 12:22:04 -0300

    array list with all the non-parsed petri matrices

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,11 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,13 +23,42 @@ public class main {
             }
         }
 
-        Element firstTable = doc.select("table").first();
-        System.out.print(firstTable.text());
+        //Element firstTable = doc.select("table").first();
+        //System.out.print(firstTable.text());
+        //System.out.println("\n\n\n---------\n\n");
 
-        System.out.println("\n\n\n---------\n\n\n");
+        ArrayList<String> tableList = new ArrayList<>();
+        Elements tables = null;
+
+        try{
+            tables = doc.getElementsByTag("table");
+        }
+        catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+        int nTable = 0;
+        for(Element table : tables){
+
+            if(nTable%2 == 0){
+                tableList.add(table.text());
+            }
+
+            nTable++;
+        }
+
+        for (int i=0; i<tableList.size(); i++){
+            parseMatrix(tableList.get(i));
+        }
+
+    }
+
+    static private void parseMatrix(String plainText){
+
+        System.out.println("\n\n\n---------\n\n");
 
         Pattern pattern = Pattern.compile("P\\d");
-        Matcher matcher = pattern.matcher(firstTable.text());
+        Matcher matcher = pattern.matcher(plainText);
 
         int nPlaces = 0;
         int nTrans = 0;
@@ -37,31 +68,30 @@ public class main {
         }
 
         pattern = Pattern.compile("T\\d");
-        matcher = pattern.matcher(firstTable.text());
+        matcher = pattern.matcher(plainText);
 
         while (matcher.find()){
             nTrans++;
         }
 
         pattern = Pattern.compile("\\s\\d+");
-        matcher = pattern.matcher(firstTable.text());
+        matcher = pattern.matcher(plainText);
 
         for(int j=0; j<nPlaces; j++){
 
             for(int i=0; i<nTrans; i++){
                 matcher.find();
-                System.out.print(matcher.group()+" ");
+                System.out.print(matcher.group());
             }
 
             System.out.println("");
         }
 
+        System.out.println("");
+
 
         System.out.println("Cantidad de Plazas = "+ String.valueOf(nPlaces));
         System.out.println("Cantidad de Transiciones = "+ String.valueOf(nTrans));
-
-
-
 
     }
 
