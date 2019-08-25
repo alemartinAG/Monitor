e49d42fe030802b8ede562aad7f2aa17bf2e4bba
alemartinAG <ale.martin96@hotmail.com>
2019-08-25 13:46:07 -0300

    petri net class

---
+++ src/com/main/main.java
@@ -0,0 +1,25 @@
+package com.main;
+
+import com.monitor.PetriNet;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class main {
+
+    public static void main(String[] args){
+
+        PetriNet petriNet = new PetriNet();
+
+    }
+
+
+
+}
---
+++ src/com/monitor/PetriNet.java
@@ -0,0 +1,110 @@
+package com.monitor;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class PetriNet {
+
+    private static final int FIM = 0; // Forwards Incidence Matrix (I+)
+    private static final int BIM = 1; // Backwards Incidence Matrix (I-)
+    private static final int CIM = 2; // Combined Incidence Matrix (I)
+    private static final int INM = 3; // Inhibition Matrix (H)
+    private static final int MRK = 4; // Marking
+    private static final int ETR = 5; // Enabled Transitions
+
+    private int nPlaces = 0;
+    private int nTransitions = 0;
+
+    private ArrayList<String> tableList;
+
+    public PetriNet(){
+
+        File input = new File("/home/ale/Desktop/papa.html"); //get env $HOME
+
+        Document doc = null;
+        Elements tables = null;
+        tableList = new ArrayList<>();
+
+        try {
+            doc = Jsoup.parse(input, "UTF-8", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            tables = doc.getElementsByTag("table");
+        }
+        catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+        int nTable = 0;
+        for(Element table : tables){
+
+            if(nTable%2 == 0){
+                tableList.add(table.text());
+            }
+
+            nTable++;
+        }
+
+        countPT();
+
+        parseMatrix(tableList.get(CIM), nPlaces, nTransitions);
+    }
+
+    private void countPT(){
+
+        int p = 0;
+        int t = 0;
+
+        Pattern pattern = Pattern.compile("P\\d");
+        Matcher matcher = pattern.matcher(tableList.get(FIM));
+
+        while (matcher.find()) {
+            p++;
+        }
+
+        pattern = Pattern.compile("T\\d");
+        matcher = pattern.matcher(tableList.get(FIM));
+
+        while (matcher.find()){
+            t++;
+        }
+
+        nPlaces = p;
+        nTransitions = t;
+
+        System.out.println("sdadsdaad de Plazas = "+ String.valueOf(nPlaces));
+        System.out.println("Cantidad de Transiciones = "+ String.valueOf(nTransitions));
+    }
+
+    private void parseMatrix(String plainText, int rows, int columns){
+
+        System.out.println(plainText);
+
+        Pattern pattern = Pattern.compile("\\s-?\\d+");
+        Matcher matcher = pattern.matcher(plainText);
+
+        for(int j=0; j<rows; j++){
+            for(int i=0; i<columns; i++){
+                matcher.find();
+                System.out.print(matcher.group());
+            }
+            System.out.println("");
+        }
+
+        System.out.println("");
+    }
+
+}
